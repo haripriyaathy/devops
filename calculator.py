98583cf69a6a7161d2e997f73ec2adec194d5a92
Haripriya <haripriyaathy@gmail.com>
2026-09-09 19:49:25 +0530

    Updated calculator

--- calculator.py
+++ calculator.py
@@ -1,13 +1,8 @@
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    if b == 0:
-        raise ValueError("Division by zero is not allowed")
-    return a / b
+class T:
+    def show(self):
+        print('this is T')
+class R(T):
+    def show(self):
+        print('this is R')
+t1=R()
+print(t1.show())
